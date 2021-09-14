2b6b9346d81e67ee405d2afc0d6522cad545a05a
waltermas <walterjmas@gmail.com>
2021-09-14 18:42:47 -0300

    bootstrap example

--- processWebHook.py
+++ processWebHook.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,26 @@ def user(name):
 def hellouser(name):
     return render_template("index.html", name=name)
 
+@app.route('/trythis')
+def trythis():
+    return render_template("trythis.html")
+
+@app.route('/login', methods = ["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        user = request.form["nm"]
+        print(user)
+        return redirect(url_for("userlogin", usergin=user))
+    else:
+        return render_template("login.html")
+
+@app.route('/<usergin>')
+def userlogin(userlogin):
+    return (f"hello {userlogin}")
+
+@app.route('/bootstrap')
+def bootstrap():
+    return render_template("bootstrap.html")
 
 if __name__ == '__main__':
     app.debug = True
